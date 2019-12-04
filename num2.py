870c8b95681be9530b394dc774d6fe9c3e90006f
pineapple79 <fun.goul.109@gmail.com>
2019-12-04 09:03:38 +0600

    Task4-2

--- num2.py
+++ num2.py
@@ -13,15 +13,64 @@
 # все методы класса.
 
 
- class Airline:
-     def __init__(self,mark,model,year,max_speed,odometer,is_flying = False)
+class Airplane:
+
+     def __init__(self,mark,model,year,max_speed):
          self.mark = mark
          self.model = model
          self.year = year
          self.max_speed = max_speed
-         self.odometer = odometer
-         self.is_flying = is_flying
+         self.odometer = 0
+         self.is_flying = False
 
-        def take_off
-        def fly
-        def land
+     def take_off(self):
+        self.is_flying = True
+        message_take = f"{self.mark} {self.model} was take off."
+        return message_take
+
+     def fly(self, km):
+        self.odometer += km
+        message_fly = f"{self.mark}f lew {self.odometer}km during the flying {self.max_speed} km/h."
+        return message_fly
+
+     def land(self):
+            self.is_flying = False
+            message_land = f"{self.mark} landed, the odometer shows {self.odometer}km."
+            return message_land
+start = Airplane("Sukhoi","Su-27","2018",2500)
+print(start.take_off())
+print(start.fly(600))
+print(start.fly(600))
+print(start.land())
+
+
+# class Airplane():
+#
+#     def __init__(self, make, model, year, max_speed):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.max_speed = max_speed
+#         self.odometer = 0
+#         self.is_flying = False
+#
+#     def take_off(self):
+#         self.is_flying = True
+#         message_take = f"{self.make} {self.model} was take off."
+#         return message_take
+#
+#     def fly(self, km):
+#         self.odometer += km
+#         message_fly = f"{self.make} flew {self.odometer}km at a speed of {self.max_speed}km/h."
+#         return message_fly
+#
+#     def land(self):
+#         self.is_flying = False
+#         message_land = f"{self.make} has landed, it's odometer shows {self.odometer}km."
+#         return message_land
+#
+# go = Airplane("Boeing", "747", "2019", 600)
+# print(go.take_off())
+# print(go.fly(500))
+# print(go.fly(500))
+# print(go.land())
